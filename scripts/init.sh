06e73ea28cd70d959e7c6a4a4dd2a1191806f913
50bytes.dev <50bytes.dev@gmail.com>
2025-03-05 17:17:04 +0400

    fix: Remove conda environment creation and activation from initialization script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,3 @@
-conda env create -f environment.yaml
-conda activate qwen2
 pip install qwen-vl-utils
 pip install git+https://github.com/huggingface/transformers.git@9d2056f12b66e64978f78a2dcb023f65b2be2108 accelerate
 MAX_JOBS=20 pip install -v flash-attn==2.5.8 --no-build-isolation
