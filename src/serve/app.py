61045e8c56acb7c91f5b8c61b7b61982f6d89dbf
50bytes.dev <50bytes.dev@gmail.com>
2025-03-05 18:36:21 +0400

    style: Refactor code for improved readability and consistency in app.py

--- src/serve/app.py
+++ src/serve/app.py
@@ -2,7 +2,11 @@ import argparse
 from threading import Thread
 import gradio as gr
 from PIL import Image
-from src.utils import load_pretrained_model, get_model_name_from_path, disable_torch_init
+from src.utils import (
+    load_pretrained_model,
+    get_model_name_from_path,
+    disable_torch_init,
+)
 from transformers import TextIteratorStreamer
 from functools import partial
 import warnings
@@ -10,9 +14,20 @@ from qwen_vl_utils import process_vision_info
 
 warnings.filterwarnings("ignore")
 
+
 def is_video_file(filename):
-    video_extensions = ['.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv', '.webm', '.mpeg']
+    video_extensions = [
+        ".mp4",
+        ".avi",
+        ".mkv",
+        ".mov",
+        ".wmv",
+        ".flv",
+        ".webm",
+        ".mpeg",
+    ]
     return any(filename.lower().endswith(ext) for ext in video_extensions)
+
 
 def bot_streaming(message, history, generation_args):
     # Initialize variables
@@ -40,7 +55,9 @@ def bot_streaming(message, history, generation_args):
                 file_paths = [file_paths]
             for file_path in file_paths:
                 if is_video_file(file_path):
-                    user_content.append({"type": "video", "video": file_path, "fps":1.0})
+                    user_content.append(
+                        {"type": "video", "video": file_path, "fps": 1.0}
+                    )
                 else:
                     user_content.append({"type": "image", "image": file_path})
             if user_text:
@@ -57,19 +74,39 @@ def bot_streaming(message, history, generation_args):
     for image in images:
         user_content.append({"type": "image", "image": image})
     for video in videos:
-        user_content.append({"type": "video", "video": video, "fps":1.0})
-    user_text = message['text']
+        user_content.append({"type": "video", "video": video, "fps": 1.0})
+    user_text = message["text"]
     if user_text:
         user_content.append({"type": "text", "text": user_text})
     conversation.append({"role": "user", "content": user_content})
 
-    prompt = processor.apply_chat_template(conversation, tokenize=False, add_generation_prompt=True)
+    prompt = processor.apply_chat_template(
+        conversation, tokenize=False, add_generation_prompt=True
+    )
     image_inputs, video_inputs = process_vision_info(conversation)
-    
-    inputs = processor(text=[prompt], images=image_inputs, videos=video_inputs, padding=True, return_tensors="pt").to(device) 
 
-    streamer = TextIteratorStreamer(processor.tokenizer, **{"skip_special_tokens": True, "skip_prompt": True, 'clean_up_tokenization_spaces':False,}) 
-    generation_kwargs = dict(inputs, streamer=streamer, eos_token_id=processor.tokenizer.eos_token_id, **generation_args)
+    inputs = processor(
+        text=[prompt],
+        images=image_inputs,
+        videos=video_inputs,
+        padding=True,
+        return_tensors="pt",
+    ).to(device)
+
+    streamer = TextIteratorStreamer(
+        processor.tokenizer,
+        **{
+            "skip_special_tokens": True,
+            "skip_prompt": True,
+            "clean_up_tokenization_spaces": False,
+        },
+    )
+    generation_kwargs = dict(
+        inputs,
+        streamer=streamer,
+        eos_token_id=processor.tokenizer.eos_token_id,
+        **generation_args,
+    )
 
     thread = Thread(target=model.generate, kwargs=generation_kwargs)
     thread.start()
@@ -79,38 +116,47 @@ def bot_streaming(message, history, generation_args):
         buffer += new_text
         yield buffer
 
-def main(args):
 
+def main(args):
     global processor, model, device
 
     device = args.device
-    
+
     disable_torch_init()
 
     use_flash_attn = True
-    
+
     model_name = get_model_name_from_path(args.model_path)
-    
+
     if args.disable_flash_attention:
         use_flash_attn = False
 
-    processor, model = load_pretrained_model(model_base = args.model_base, model_path = args.model_path, 
-                                                device_map=args.device, model_name=model_name, 
-                                                load_4bit=args.load_4bit, load_8bit=args.load_8bit,
-                                                device=args.device, use_flash_attn=use_flash_attn
+    processor, model = load_pretrained_model(
+        model_base=args.model_base,
+        model_path=args.model_path,
+        device_map=args.device,
+        model_name=model_name,
+        load_4bit=args.load_4bit,
+        load_8bit=args.load_8bit,
+        device=args.device,
+        use_flash_attn=use_flash_attn,
     )
 
     chatbot = gr.Chatbot(scale=2)
-    chat_input = gr.MultimodalTextbox(interactive=True, file_types=["image", "video"], placeholder="Enter message or upload file...",
-                                  show_label=False)
-    
+    chat_input = gr.MultimodalTextbox(
+        interactive=True,
+        file_types=["image", "video"],
+        placeholder="Enter message or upload file...",
+        show_label=False,
+    )
+
     generation_args = {
         "max_new_tokens": args.max_new_tokens,
         "temperature": args.temperature,
         "do_sample": True if args.temperature > 0 else False,
         "repetition_penalty": args.repetition_penalty,
     }
-    
+
     bot_streaming_with_args = partial(bot_streaming, generation_args=generation_args)
 
     with gr.Blocks(fill_height=True) as demo:
@@ -123,9 +169,9 @@ def main(args):
             chatbot=chatbot,
         )
 
-
     demo.queue(api_open=False)
-    demo.launch(show_api=False, share=False, server_name='0.0.0.0')
+    demo.launch(show_api=False, share=False, server_name="0.0.0.0", port=8888)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
