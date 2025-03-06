8f0fd46fc2557178390c9e9cc8cf05648e561faf
50bytes.dev <50bytes.dev@gmail.com>
2025-03-06 21:15:14 +0400

    fix: Update AutoAWQ repository URL in init.sh to new maintainer

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,4 @@
-pip install git+https://github.com/seungwoos/AutoAWQ.git@23d584c25d80381671293d2f08a9fadfa5a8986a
+pip install git+https://github.com/casper-hansen/AutoAWQ.git@23d584c25d80381671293d2f08a9fadfa5a8986a
 pip install qwen-vl-utils
 pip install git+https://github.com/huggingface/transformers.git@bc30dd1efb99f571d45b2e2131a555d09285ddd8 accelerate
 MAX_JOBS=20 pip install -v flash-attn==2.5.8 --no-build-isolation
