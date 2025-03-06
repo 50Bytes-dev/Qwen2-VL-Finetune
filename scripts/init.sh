f1ee7f97268a49ca49114d5bee79a4ffaa9acbcb
50bytes.dev <50bytes.dev@gmail.com>
2025-03-06 18:58:48 +0400

    fix: Update dependencies in init.sh; add AutoAWQ and update transformers version

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,4 @@
+pip install git+https://github.com/50Bytes-dev/AutoAWQ.git@e297dc5c665eb38b5d741e2ed7e22def646912eb
 pip install qwen-vl-utils
-pip install git+https://github.com/huggingface/transformers.git@9d2056f12b66e64978f78a2dcb023f65b2be2108 accelerate
+pip install git+https://github.com/huggingface/transformers.git@bc30dd1efb99f571d45b2e2131a555d09285ddd8 accelerate
 MAX_JOBS=20 pip install -v flash-attn==2.5.8 --no-build-isolation
