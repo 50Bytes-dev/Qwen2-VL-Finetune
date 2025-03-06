5a5622c0497c40166e0cfa4f5d9973e11ec77e0e
50bytes.dev <50bytes.dev@gmail.com>
2025-03-06 21:50:06 +0400

    fix: Update script to use awq.py for weight merging

--- scripts/awq.sh
+++ scripts/awq.sh
@@ -2,7 +2,7 @@
 
 export PYTHONPATH=src:$PYTHONPATH
 
-python src/merge_lora_weights.py \
+python src/awq.py \
     --model-path /root/Qwen2-VL-Finetune/output/qwen_2.5_merge \
     --quant-path /root/Qwen2-VL-Finetune/output/qwen_2.5_awq \
     --token my_hf_token
